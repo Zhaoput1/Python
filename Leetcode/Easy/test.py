abd0d3bac0f38769007516fb4e496ead9c023352
Zhaoput1 <71040636+Zhaoput1@users.noreply.github.com>
2021-02-16 20:32:09 -0500

    Add files via upload

---
+++ Leetcode/Easy/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2/16/21 
+
+# @Author  : Zhaopu Teng
+"""
+
+nums = [1,3,4,2]
+hashmap = {}
+for index, num in enumerate(nums):
+    hashmap[num] = index
+print(hashmap)
---
+++ Leetcode/Easy/twoSum.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2/16/21 
+
+# @Author  : Zhaopu Teng
+"""
+
+def twoSum(nums, tar):
+    hashmap = {}
+    for index, num in enumerate(nums):
+        hashmap[num] = index
+    for i, num in enumerate(nums):
+        j = hashmap.get(tar - num)
+        if j is not None and i !=j:
+            return [i,j]
+
+nums = [2,3,4,5,1,9,7,11]
+tar = 7
+print(twoSum(nums, tar))
+ 
